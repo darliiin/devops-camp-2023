ca69c5ceec24ab7ddc7e2e2ed758a817290e22d0
darliiin <dashuta777777@mail.ru>
2023-03-30 22:39:29 +0500

    Changes made to the chapter2-t2-gen-customization.sh file

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#generating ssh keys for passed repositories, generating repos/customization.yaml
 if [[ $# -eq 0 ]]; then
     echo "Error: No arguments provided."
     exit 1
@@ -8,20 +8,18 @@ fi
 if [ ! -d "repos" ]; then
   mkdir repos
 fi
+
 # Loop through all repository names
 for repo_name in "$@"
 do
   # Generate SSH key
-  ssh-keygen -t ed25519 -f repos/"${repo_name}-deploy-key.pem" -N ""
+  ssh-keygen -y -t ed25519 -f repos/"${repo_name}-deploy-key.pem" -N "" -q
 
-  # Generate kustomization.yaml file
-  cat << EOF > repos/kustomization.yaml
-apiVersion: kustomize.config.k8s.io/v1beta1
+# Generate kustomization.yaml file
+txt="apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
-
 generatorOptions:
   disableNameSuffixHash: true
-
 secretGenerator:
   - name: ${repo_name}
     namespace: argo-cd
@@ -34,9 +32,12 @@ secretGenerator:
       - type=git
       - project=default
     files:
-      - sshPrivateKey=${repo_name}-deploy-key.pem
-EOF
+      - sshPrivateKey=${repo_name}-deploy-key.pem"
+
+echo "$txt" > repos/kustomization.yaml
+
+kustomize build repos
+
 done
 
-# Build kustomization.yaml file and output to stdout
-kustomize build repos
+
