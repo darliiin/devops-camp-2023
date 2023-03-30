16c1264f38bd7f85acc65e0094d5863743c43a83
darliiin <dashuta777777@mail.ru>
2023-03-30 18:19:53 +0500

    Added chapter2-l2-t4-files-no-extensions file to t4 folder

---
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#checking for the number of passed arguments
+if [[ $# -ne 1 ]]; then
+  echo "You must specify one folder"
+  exit 1
+fi
+#search folder contents, output content without extensionsearch folder contents, output content without extension
+find "$1" -type f | while read file; do
+  echo ${file/.*/ }
+done
