db57d82139ead43e7bcccf5331a07175d1f1e4bc
darliiin <dashuta777777@mail.ru>
2023-03-30 18:50:52 +0500

    Added chapter2-l2-t5-get-file-unique-extensions.sh file to t5 folder

---
+++ chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "You must specify one folder"
+    exit 1
+fi
+
+arr=()
+
+for file in $(find $1 -type f); do
+    if [[ $file == *.* && ! "${arr[@]}" =~ ${file/*./} ]]; then
+        arr+=${file/*./}
+        echo ${file/*./}
+    fi
+done
+
+# echo ${arr[@]}
