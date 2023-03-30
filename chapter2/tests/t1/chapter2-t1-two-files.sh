3371dddae20aa9e6bea137e3e588da2d7205134d
darliiin <dashuta777777@mail.ru>
2023-03-30 18:03:10 +0500

    Changes made to the chapter2-t1-two-files.sh file

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -1,39 +1,22 @@
 #!/bin/bash
-
-if [[ "$#" = 2 ]]; then
-    file1="$1"
-    file2="$2"
-    if [[ -f "$file1" ]] && [[ -f "$file2" ]]; then
-        echo "$file1:"
-        cat "$file1"
-        echo "$file2:"
-        cat "$file2"
-    elif [[ ! -f "$file1" ]] && [[ ! -f "$file2" ]]; then
-        rstring1=$(openssl rand -base64 8)
-        rstring2=$(openssl rand -base64 8)
-        echo $rstring1 | base64 -d > "$file1"
-        echo $rstring2 | base64 -d > "$file2"
-        chmod 700 "$file1" "$file2"
-        echo "new files: $file1, $file2"
-    elif [[ ! -f "$file1" ]]; then
-        rstring1=$(openssl rand -base64 8)
-        echo $rstring1 | base64 -d > "$file1"
-        chmod 700 "$file1"
-        echo "new file: $file1"
-        echo "$file2:"
-        cat "$file2"
-    else
-        echo "$file1:"
-        cat "$file1"
-        rstring2=$(openssl rand -base64 8)
-        echo $rstring2 | base64 -d > "$file2"
-        chmod 700 "$file2"
-        echo "new file: $file2"
-    fi
-
-else
-    echo "Enter two arguments"
-    exit 1
+#check for a valid number of arguments
+if [[ "$#" != 2 ]]; then
+  echo "Enter two arguments"
+  exit 1
 fi
-
+#check if file exists
+for file in "$1" "$2"; do
+  #if the file does not exist
+  if [[ ! -f "$file" ]]; then
+    rstring=$(openssl rand -base64 8)
+    echo "$rstring" | base64 -d > "$file"
+    chmod 700 "$file"
+    echo "new file: $file"
+  #if the file exists
+  else
+    echo "$file:"
+    cat "$file"
+    echo ""
+  fi
+done
 
