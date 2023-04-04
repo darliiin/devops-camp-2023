13223961d49e2b8fe052295089c142fc415b58ae
darliiin <dashuta777777@mail.ru>
2023-04-04 11:28:42 +0500

    Changes made to the chapter2-t2-gen-kustomization.sh file

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-#generating ssh keys for passed repositories, generating repos/customization.yaml
+
+#checking for the number of passed arguments
 if [[ $# -eq 0 ]]; then
     echo "Error: No arguments provided."
     exit 1
 fi
 
-if [ ! -d "repos" ]; then
-  mkdir repos
+KEYS_PATH="repos"
+
+if [ ! -d "$KEYS_PATH" ]; then
+  mkdir "$KEYS_PATH"
 fi
+
+#creating a repos folder if it's missing
+KUSTOMIZATION_PATH="$KEYS_PATH/kustomization.yaml"
+
+#clearing the file before writing
+echo "" > "$KUSTOMIZATION_PATH"
+
 
 # Loop through all repository names
 for repo_name in "$@"
 do
-  # Generate SSH key
-  ssh-keygen -y -t ed25519 -f repos/"${repo_name}-deploy-key.pem" -N "" -q
+  #Generate SSH key
+  echo y | ssh-keygen -t ed25519 -f $KEYS_PATH/"${repo_name}"-deploy-key.pem -N "" -q
 
 # Generate kustomization.yaml file
 txt="apiVersion: kustomize.config.k8s.io/v1beta1
@@ -34,10 +44,7 @@ secretGenerator:
     files:
       - sshPrivateKey=${repo_name}-deploy-key.pem"
 
-echo "$txt" > repos/kustomization.yaml
-
-kustomize build repos
+echo "$txt" >> "$KUSTOMIZATION_PATH"
+kustomize build "$KEYS_PATH"
 
 done
-
-
