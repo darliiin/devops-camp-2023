55e45825d1aced0eb27aab1cf0652997c4e72a0e
darliiin <dashuta777777@mail.ru>
2023-04-05 15:17:16 +0500

    Changes made to the chapter2-t2-gen-kustomization.sh file

--- chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
+++ chapter2/tests/t2/chapter2-t2-gen-kustomization.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-#checking for the number of passed arguments
+# checking for the number of passed arguments
 if [[ $# -eq 0 ]]; then
     echo "Error: No arguments provided."
     exit 1
 fi
 
 KEYS_PATH="repos"
+KUSTOMIZATION_PATH="$KEYS_PATH/kustomization.yaml"
 
 if [ ! -d "$KEYS_PATH" ]; then
   mkdir "$KEYS_PATH"
 fi
 
-#creating a repos folder if it's missing
-KUSTOMIZATION_PATH="$KEYS_PATH/kustomization.yaml"
+# clearing the file before writing
+txt0="apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+generatorOptions:
+  disableNameSuffixHash: true
+secretGenerator:"
 
-#clearing the file before writing
-echo "" > "$KUSTOMIZATION_PATH"
-
+echo "$txt0" > "$KUSTOMIZATION_PATH"
 
 # Loop through all repository names
 for repo_name in "$@"
 do
-  #Generate SSH key
+  # Generate SSH key
   echo y | ssh-keygen -t ed25519 -f $KEYS_PATH/"${repo_name}"-deploy-key.pem -N "" -q
 
 # Generate kustomization.yaml file
-txt="apiVersion: kustomize.config.k8s.io/v1beta1
-kind: Kustomization
-generatorOptions:
-  disableNameSuffixHash: true
-secretGenerator:
+txt="
   - name: ${repo_name}
     namespace: argo-cd
     options:
@@ -44,7 +43,10 @@ secretGenerator:
     files:
       - sshPrivateKey=${repo_name}-deploy-key.pem"
 
-echo "$txt" >> "$KUSTOMIZATION_PATH"
-kustomize build "$KEYS_PATH"
+cat << EOF >> $KUSTOMIZATION_PATH
+$txt
+EOF
 
 done
+
+kustomize build "$KEYS_PATH"
