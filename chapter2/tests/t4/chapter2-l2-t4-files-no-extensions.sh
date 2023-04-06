f695fbd16ce7849178a97e6378c1ae6bb1cb7008
darliiin <dashuta777777@mail.ru>
2023-04-06 14:37:20 +0500

    Changes made to the chapter2-l2-t4-files-no-extensions.sh file

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-#checking for the number of passed arguments
+# checking for the number of passed arguments
 if [[ $# -ne 1 ]]; then
   echo "You must specify one folder"
   exit 1
 fi
 
-#search folder contents, output content without extension
-find "$1" -type f | while read file; do
-  echo "${file/.*/ }"
-done
+# search folder contents, output content without extension
+find "$1" -depth -type f | while read file; do
+  if [[ $(echo "$file" | grep -o "\." | wc -l) -eq 1 && "${file##*/}" == .* ]]; then
+    echo "$file"
+  else
+    echo "${file%.*}"
+  fi
 
+done
