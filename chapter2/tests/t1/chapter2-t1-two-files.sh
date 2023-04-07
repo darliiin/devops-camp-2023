305e4970299697632af8892d41376a1c981cd4e3
darliiin <dashuta777777@mail.ru>
2023-04-07 16:20:28 +0500

    Changes made to the chapter2-t1-two-files.sh file

--- chapter2/tests/t1/chapter2-t1-two-files.sh
+++ chapter2/tests/t1/chapter2-t1-two-files.sh
@@ -1,23 +1,36 @@
-
 #!/bin/bash
-#check for a valid number of arguments
+
+# check for a valid number of arguments
 if [[ "$#" != 2 ]]; then
   echo "Enter two arguments"
   exit 1
 fi
-#check if file exists
+
+# check if file exists
 for file in "$@"; do
-  #if the file does not exist
+
+  if [[ "$file" == */* ]]; then
+
+    # extract directory path
+    dir="${file%/*}"
+
+    # create directory if it does not exist
+    if [[ ! -d "$dir" ]]; then
+      mkdir -p "$dir"
+    fi
+  fi
+
+  # if the file does not exist
   if [[ ! -f "$file" ]]; then
     rstring=$(openssl rand -base64 8)
     echo "$rstring" | base64 > "$file"
     chmod 700 "$file"
     echo "new file: $file"
-  #if the file exists
+
+  # if the file exists
   else
     echo "$file:"
-    cat "$file"
+    cat "$file" 2>/dev/null
     echo ""
   fi
 done
-
