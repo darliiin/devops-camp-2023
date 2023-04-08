80f66c1d850eaf0b19f8fd7d968e2105307942f3
darliiin <dashuta777777@mail.ru>
2023-04-08 11:03:14 +0500

    Changes made to the chapter2-l2-t4-files-no-extensions.sh file

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Script get a folder as an argument and return all files (including in the nested directories) but without the extension
+
 # checking for the number of passed arguments
 if [[ $# -ne 1 ]]; then
   echo "You must specify one folder"
@@ -7,11 +9,27 @@ if [[ $# -ne 1 ]]; then
 fi
 
 # search folder contents, output content without extension
-find "$1" -depth -type f | while read file; do
-  if [[ $(echo "$file" | grep -o "\." | wc -l) -eq 1 && "${file##*/}" == .* ]]; then
-    echo "$file"
-  else
-    echo "${file%.*}"
+find "$1" -type f | while read file; do
+
+  # check files that contain only one dot in the name
+  if [[ $(echo "$file" | grep -o "\." | wc -l) -eq 1 ]]; then
+
+    # if the file is hidden, then display the file name
+    if [[ "${file}" == *"/."* ]]; then
+      echo "${file}"
+    else
+      echo "${file%.*}"
+    fi
+
+  # check files that contain zero dots in the name
+  elif
+    [[ $(echo "$file" | grep -o "\." | wc -l) -eq 0 ]]; then
+      echo "$file"
+
+  # check files that contain more than one dot in the name
+  elif
+    [[ $(echo "$file" | grep -o "\." | wc -l) > 1 ]]; then
+        echo "${file%.*}"
   fi
 
 done
