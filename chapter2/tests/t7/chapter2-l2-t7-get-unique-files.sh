6c3c5cdbc0604b764a593c10dc473a005dd1693c
darliiin <dashuta777777@mail.ru>
2023-04-13 10:51:39 +0500

    Changes made to the chapter2-l2-t7-get-unique-files.sh

--- chapter2/tests/t7/chapter2-l2-t7-get-unique-files.sh
+++ chapter2/tests/t7/chapter2-l2-t7-get-unique-files.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-#Checking the number of arguments passed
+
+# getting a list of all unique files with extensions but no absolute path
+
+# сhecking the number of arguments passed
 if [[ $# -ne 1 ]]; then
   echo "You must specify one folder"
   exit 1
 fi
-#create an array
-arr=()
-#return a list of all unique files with extensions without the absolute path
-for file in $(find $1 -type f); do
-  if [[ ! "${arr[@]}" =~ ${file##*/} ]]; then
-    arr+=${file##*/}
-    echo ${file##*/}
+
+var=$1
+
+# create an array
+file_name=()
+
+# return a list of all unique files with extensions without the absolute path
+find "$var" -type f | while read -r file; do
+  if [[ ! "${file_name[@]}" =~ "${file##*/}" ]]; then
+    file_name+="${file##*/}"
+    echo "${file##*/}"
   fi
 done
