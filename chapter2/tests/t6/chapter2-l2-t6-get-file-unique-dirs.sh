84df5f5dca10e129558e2b929fbbda162984883b
darliiin <dashuta777777@mail.ru>
2023-04-14 18:58:27 +0500

    Changes made to the chapter2-l2-t6-get-file-unique-dirs.sh file

--- chapter2/tests/t6/chapter2-l2-t6-get-file-unique-dirs.sh
+++ chapter2/tests/t6/chapter2-l2-t6-get-file-unique-dirs.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-#Checking the number of arguments passed
+
+# Checking the number of arguments passed
 if [[ $# -ne 1 ]]; then
   echo "You must specify one folder"
   exit 1
 fi
-#return a list of unique directories of all files
-for file in $(find $1 -type f); do
-  if [[ ! "${arr[@]}" =~ $file ]]; then
-    arr+=$file
-    echo $file
-  fi
-done
 
+folder=$1
 
-
-
-
-
+# return a list of unique directories of all files
+for file in $(find $folder -type f); do
+  echo "${file%/*}"
+done | sort -u
