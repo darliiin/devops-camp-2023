fa06b0e37bede07abde5c6c40bdebb91764b8083
darliiin <dashuta777777@mail.ru>
2023-04-14 17:41:27 +0500

    Changes made to the chapter2-l2-t7-get-unique-files.sh

--- chapter2/tests/t7/chapter2-l2-t7-get-unique-files.sh
+++ chapter2/tests/t7/chapter2-l2-t7-get-unique-files.sh
@@ -8,15 +8,10 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
-var=$1
+folder=$1
 
-# create an array
-file_name=()
-
-# return a list of all unique files with extensions without the absolute path
-find "$var" -type f | while read -r file; do
-  if [[ ! "${file_name[@]}" =~ "${file##*/}" ]]; then
-    file_name+="${file##*/}"
-    echo "${file##*/}"
-  fi
-done
+# all unique files with extensions without the absolute path
+find "$folder" -type f | while read -r file; do
+  file_name="${file##*/}"
+  echo "${file_name}"
+done | sort -u
