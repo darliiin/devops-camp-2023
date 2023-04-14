927c8541e22921d72fc8d2e608317eaf8c85b37d
darliiin <dashuta777777@mail.ru>
2023-04-14 14:55:07 +0500

    Changes made to the chapter2-l2-t4-files-no-extensions.sh file

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -9,12 +9,12 @@ if [[ $# -ne 1 ]]; then
 fi
 
 # search folder contents, output content without extension
-find "$1" -type f | while read file; do
+find "$1" -type f | while read -r file; do
 
   # check files that contain only one dot in the name
   if [[ $(echo "$file" | grep -o "\." | wc -l) -eq 1 ]]; then
 
-    # if the file is hidden, then display the file name
+    # if the file is dotfile
     if [[ "${file}" == *"/."* ]]; then
       echo "${file}"
     else
@@ -22,13 +22,11 @@ find "$1" -type f | while read file; do
     fi
 
   # check files that contain zero dots in the name
-  elif
-    [[ $(echo "$file" | grep -o "\." | wc -l) -eq 0 ]]; then
+  elif [[ $(echo "$file" | grep -o "\." | wc -l) -eq 0 ]]; then
       echo "$file"
 
   # check files that contain more than one dot in the name
-  elif
-    [[ $(echo "$file" | grep -o "\." | wc -l) > 1 ]]; then
+  elif [[ $(echo "$file" | grep -o "\." | wc -l) > 1 ]]; then
       file_path=$(dirname "$file")
       file_name="${file##*/}"
       if [[ $(echo "$file_name" | grep -o "\." | wc -l) -eq 1 ]]; then
