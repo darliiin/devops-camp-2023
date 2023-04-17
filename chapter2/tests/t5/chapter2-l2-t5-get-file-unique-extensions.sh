b36724394f824ad638dd2136a2b0c1b99eebf6d5
darliiin <dashuta777777@mail.ru>
2023-04-17 14:50:54 +0500

    Changes made to the chapter2-l2-t5-get-file-unique-extensions.sh file

--- chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
+++ chapter2/tests/t5/chapter2-l2-t5-get-file-unique-extensions.sh
@@ -8,31 +8,34 @@ if [[ $# -ne 1 ]]; then
     exit 1
 fi
 
-var=$1
-
-touch unique_ext;
-echo>unique_ext
+folder=$1
 
 # search folder contents, output content without extension
-find "$var" -type f | while read file; do
-    # check files that contain only one dot in the name
-    if [[ $(echo "$file" | grep -o "\." | wc -l) -eq 1  ]]; then
-        if [[ ! "${file}" == *"/."* ]]; then
-          echo "${file/*./}" >> unique_ext
-        fi
+find "$folder" -type f | while read  file; do
 
-    # check files that contain more than one dot in the name
-    elif
-      [[ $(echo "$file" | grep -o "\." | wc -l) > 1 ]]; then
-
-        # extract filename from file
-        file_name="${file##*/}"
-
-        # if the number of dots in the file name is not equal to one, or is equal to one, but the file is not hidden
-        if [[ ($(echo "$file_name" | grep -o "\." | wc -l) -ne 1) || ($(echo "$file_name" | grep -o "\." | wc -l) -eq 1 &&  ! "${file_name}" == *"/."*) ]]; then
-          echo "${file_name#*.}" >> unique_ext
-        fi
+  # check files that contain only one dot in the name
+  if [[ $(echo "$file" | grep -o "\." | wc -l) -eq 1  ]]; then
+    if [[ ! "${file}" == *"/."* ]]; then
+      echo "${file/*./}"
     fi
-done
 
-sort unique_ext | uniq
+  # check files that contain more than one dot in the name
+  elif [[ $(echo "$file" | grep -o "\." | wc -l) > 1 ]]; then
+
+    # extract filename from file
+    file_name="${file##*/}"
+
+    # skip files without extension and hidden files without extension
+    if [[ ($(echo "$file_name" | grep -o "\." | wc -l) -eq 0) || \
+          ($(echo "$file_name" | grep -o "\." | wc -l) -eq 1 &&  "${file_name:0:1}" == ".") ]]; then
+      continue
+
+    # write the extension to the file if the number of points is not equal to 1 or equal to one
+    # but the file is not hidden
+    elif [[ ($(echo "$file_name" | grep -o "\." | wc -l) -eq 1 &&  ! "${file_name:0:1}" == ".") ||
+            ($(echo "$file_name" | grep -o "\." | wc -l) -ne 1) ]]; then
+      echo "${file_name#*.}"
+    fi
+  fi
+
+done | sort -u
