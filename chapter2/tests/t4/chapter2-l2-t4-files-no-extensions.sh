5d4d05892ab1d1458f24f55adf88ec9b059be64c
darliiin <dashuta777777@mail.ru>
2023-04-19 17:32:08 +0500

    Changes made to the chapter2-l2-t4-files-no-extensions.sh file

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -14,7 +14,7 @@ find "$1" -type f | while read -r file; do
   # check files that contain only one dot in the name
   if [[ $(echo "$file" | grep -o "\." | wc -l) -eq 1 ]]; then
 
-    # if the file is dotfile
+    # if the file is hidden, then display the file name
     if [[ "${file}" == *"/."* ]]; then
       echo "${file}"
     else
@@ -22,17 +22,27 @@ find "$1" -type f | while read -r file; do
     fi
 
   # check files that contain zero dots in the name
-  elif [[ $(echo "$file" | grep -o "\." | wc -l) -eq 0 ]]; then
+  elif
+    [[ $(echo "$file" | grep -o "\." | wc -l) -eq 0 ]]; then
       echo "$file"
 
   # check files that contain more than one dot in the name
-  elif [[ $(echo "$file" | grep -o "\." | wc -l) > 1 ]]; then
+  elif
+    [[ $(echo "$file" | grep -o "\." | wc -l) > 1 ]]; then
       file_path=$(dirname "$file")
       file_name="${file##*/}"
-      if [[ $(echo "$file_name" | grep -o "\." | wc -l) -eq 1 ]]; then
-        echo "${file_path}/${file_name}"
-      else
+
+      # remove the extension if the file name: contains more than 1 dot
+      # OR the file is not hidden and has the extension
+      if [[ ($(echo "$file_name" | grep -o "\." | wc -l) -ne 1) || \
+            ($(echo "$file_name" | grep -o "\." | wc -l) -eq 1) && ! ("${file_name:0:1}" == ".") ]]; then
         echo "${file_path}/${file_name%%.*}"
+
+      # output the file name unchanged if the file does not contain dots
+      # OR it is a hidden file
+      elif [[ ($(echo "$file_name" | grep -o "\." | wc -l) -eq 0) || \
+              ($(echo "$file_name" | grep -o "\." | wc -l) -eq 1) && ("${file_name:0:1}" == ".") ]]; then
+        echo "${file_path}/${file_name}"
       fi
   fi
 
