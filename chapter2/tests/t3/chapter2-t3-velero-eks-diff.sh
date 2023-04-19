518a37a7f00a105079a007c7782d45cb7e628c32
darliiin <dashuta777777@mail.ru>
2023-04-19 17:54:04 +0500

    Changes made to the chapter2-t3-velero-eks-diff.sh file

--- chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
+++ chapter2/tests/t3/chapter2-t3-velero-eks-diff.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# list of kubernetes namespaces that are not backed up
-
 # function to check if a file was loaded or not
 function check_file() {
   local uri=$1
@@ -13,20 +11,24 @@ function check_file() {
 }
 
 # download velero backup
-VELERO_BACKUP_NAMESPACES=$(curl -sSL https://gist.github.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/velero.yaml)
+VELERO_BACKUP=$(curl -sSL https://gist.github.com/dmitry-mightydevops/016139747b6cefdc94160607f95ede74/raw/velero.yaml)
 
-check_file "velero.yaml" "${VELERO_BACKUP_NAMESPACES}"
+check_file "velero.yaml" "${VELERO_BACKUP}"
 
 # list of kubernetes namespaces backed up
-VELERO_NAMESPACES=$(echo "$VELERO_BACKUP_NAMESPACES" | yq ".spec.source.helm.values" \
+VELERO_NAMESPACES=$(echo "$VELERO_BACKUP" | yq ".spec.source.helm.values" \
   | yq ".schedules[].template.includedNamespaces[]" | sort )
 
 # list of actual kubernetes namespaces
 KUBERNETES_NAMESPACES=$(curl -sSL https://gist.github.com/dmitry-mightydevops/297c4e235b61982f21a0bbbf7319ac24/raw/kubernetes-namespaces.txt)
 
+# check if kubernetes-namespaces.txt file exists
+if [[ ! -f "kubernetes-namespaces.txt" ]]; then
+  echo "ERROR: kubernetes-namespaces.txt file not found"
+  exit 1
+fi
+
 check_file "kubernetes-namespaces.txt" "KUBERNETES_NAMESPACES"
 
 # list of kubernetes namespaces that are not backed up
-echo "$VELERO_NAMESPACES" > combined.txt
-echo "$KUBERNETES_NAMESPACES" >> combined.txt
-sort combined.txt | uniq -u
+(sort <<< "$VELERO_NAMESPACES"$'\n'"$KUBERNETES_NAMESPACES" | uniq -u)
