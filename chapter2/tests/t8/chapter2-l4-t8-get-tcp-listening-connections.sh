a8c2628648e316807ca1edab151e3421458cf4af
darliiin <dashuta777777@mail.ru>
2023-05-04 00:26:34 +0500

    Сhanged file chapter2-l4-t8-get-tcp-listening-connections.sh

--- chapter2/tests/t8/chapter2-l4-t8-get-tcp-listening-connections.sh
+++ chapter2/tests/t8/chapter2-l4-t8-get-tcp-listening-connections.sh
@@ -3,7 +3,7 @@
 ports=()
 first_port=10000
 last_port=10100
-time_sleep=5
+time_sleep=30
 
 # TCP connections in the port range 10000-10100
 while (true); do
@@ -16,12 +16,14 @@ while (true); do
   # check if we displayed information about this port
   if ! [[ "${ports[@]}" =~ "${num_port}" ]]; then
 
-    # if the connection is new, print to stdout and add the port to the arraylast_port
-    echo "New connection on port ${connections}" | awk '{print $4, $5, $6, $7}'
-    ports+="${num_port}"
-  else
-  echo "No listening TCP sockets bound to ports in the range 10000-10100"
-  fi
+    # if the connection is new, print to stdout and add the port to the 'ports' array
+    echo -e "\nNew connection on port ${connections}" | awk '{print $4, $5, $6, $7}'
+    ports+=("$num_port")
+    else
+      echo -e "\nNo listening TCP sockets bound to ports in the range ${first_port} - ${last_port}"
+      echo "${connections}" | awk '{print $4, $5, $6, $7}'
+    fi
+
 sleep "${time_sleep}";
 
 done
