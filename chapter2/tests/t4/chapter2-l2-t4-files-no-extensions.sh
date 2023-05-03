dac60933bf87601b1e6d4236d3161f5b77c2e104
darliiin <dashuta777777@mail.ru>
2023-05-04 00:51:21 +0500

    Сhanged file chapter2-l2-t4-files-no-extensions.sh file

--- chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
+++ chapter2/tests/t4/chapter2-l2-t4-files-no-extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script get a folder as an argument and return all files (including in the nested directories) but without the extension
+# Script get a folder as an argument and return all files (including in the nested directories) but without the ext>
 
 # checking for the number of passed arguments
 if [[ $# -ne 1 ]]; then
@@ -11,8 +11,10 @@ fi
 # search folder contents, output content without extension
 find "$1" -type f | while read -r file; do
 
+  file_num_dots=$(echo "$file" | grep -o "\." | wc -l)
+
   # check files that contain only one dot in the name
-  if [[ $(echo "$file" | grep -o "\." | wc -l) -eq 1 ]]; then
+  if [[ "$file_num_dots" -eq 1 ]]; then
 
     # if the file is hidden, then display the file name
     if [[ "${file}" == *"/."* ]]; then
@@ -23,25 +25,27 @@ find "$1" -type f | while read -r file; do
 
   # check files that contain zero dots in the name
   elif
-    [[ $(echo "$file" | grep -o "\." | wc -l) -eq 0 ]]; then
+    [[ "$file_num_dots" -eq 0 ]]; then
       echo "$file"
 
   # check files that contain more than one dot in the name
   elif
-    [[ $(echo "$file" | grep -o "\." | wc -l) > 1 ]]; then
+    [[ "$file_num_dots" > 1 ]]; then
       file_path=$(dirname "$file")
       file_name="${file##*/}"
 
       # remove the extension if the file name: contains more than 1 dot
       # OR the file is not hidden and has the extension
-      if [[ ($(echo "$file_name" | grep -o "\." | wc -l) -ne 1) || \
-            ($(echo "$file_name" | grep -o "\." | wc -l) -eq 1) && ! ("${file_name:0:1}" == ".") ]]; then
+      file_name_num_dots=$(echo "$file_name" | grep -o "\." | wc -l)
+
+      if [[ ("$file_name_num_dots" -ne 1) || \
+            ("$file_name_num_dots" -eq 1) && ! ("${file_name:0:1}" == ".") ]]; then
         echo "${file_path}/${file_name%%.*}"
 
       # output the file name unchanged if the file does not contain dots
       # OR it is a hidden file
-      elif [[ ($(echo "$file_name" | grep -o "\." | wc -l) -eq 0) || \
-              ($(echo "$file_name" | grep -o "\." | wc -l) -eq 1) && ("${file_name:0:1}" == ".") ]]; then
+      elif [[ ("$file_num_dots" -eq 0) || \
+              ("$file_num_dots" -eq 1) && ("${file_name:0:1}" == ".") ]]; then
         echo "${file_path}/${file_name}"
       fi
   fi
